b942fabc61a26ee7cbbcea264b551b85bd40a1f4
Hong Louis <mm9301tw@gmail.com>
2018-10-25 21:59:44 +0800

    add 1019's hw

---
+++ test1019/test1019/Repository/ClassR.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using test1019.Models;
+
+namespace test1019.Repository
+{
+    class ClassR
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mm930\source\repos\nkust1019\test1019\test1019\data\Database1.mdf;Integrated Security=True";
+            }
+        }
+        public void Insert(Class1 item)
+        {
+            var newItem = item;
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            var command = new SqlCommand("", connection);
+            command.CommandText = string.Format(@"
+            INSERT INTO OpenData(服務分類, 資料集名稱, 主要欄位說明)
+            VALUES              (N'{0}', N'{1}', N'{2}')
+            ", newItem.服務分類, newItem.資料集名稱, newItem.主要欄位說明);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
---
+++ test1019/test1019/source/ClassS.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using test1019.Models;
+
+namespace test1019.source
+{
+    class ClassS
+    {
+        public List<Class1> FindOpenData()
+        {
+            List<Class1> result = new List<Class1>();
+
+            string baseDir = Directory.GetCurrentDirectory();
+
+
+            var xml = XElement.Load(@"C:/Users/mm930/source/repos/nkust1019/test1019/test1019/data/datagovtw_dataset_20181025.xml");
+
+
+            //XNamespace gml = @"http://www.opengis.net/gml/3.2";
+            //XNamespace twed = @"http://twed.wra.gov.tw/twedml/opendata";
+            var nodes = xml.Descendants("node").ToList();
+
+            result = nodes
+                .Where(x => !x.IsEmpty).ToList()
+                .Select(node =>
+                {
+                    Class1 item = new Class1();
+                    item.id = int.Parse(getValue(node, "id"));
+                    item.資料集名稱 = getValue(node, "資料集名稱");
+                    item.服務分類 = getValue(node, "服務分類");
+                    item.主要欄位說明 = getValue(node, "主要欄位說明");
+                    return item;
+                }).ToList();
+            return result;
+        }
+        public void ImportToDb(List<Class1> openDatas)
+        {
+            Repository.ClassR Repository = new Repository.ClassR();
+            openDatas.ForEach(item =>
+            {
+                Repository.Insert(item);
+            });
+        }
+        private string getValue(XElement node, string propertyName)
+        {
+            return node.Element(propertyName)?.Value?.Trim();
+        }
+    }
+}
